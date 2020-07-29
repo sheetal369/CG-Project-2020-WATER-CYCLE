45a9296bbefedfb8c4bf15b0813b8f8342c90e95
Sheetal Sharma <sheetalsharma0719@gmail.com>
2020-07-29 18:55:58 +0545

    UPDATE : Sky, Grass, Ocean, Sun

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 /* Global variables */
 char title[] = "[CG Project] WATER CYCLE";
 int refreshMills = 360;        // refresh interval in milliseconds
+GLfloat z = -2.0;
+
+GLfloat sunRadius = 0.115;
+GLfloat dx = 0;
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -17,11 +21,52 @@ void initGL() {
    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
 
+void Cloud()
+{
+
+
+}
 void display() {
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
 
-   //draw here
+   //SUN
+   glLoadIdentity();                 // Reset the model-view matri
+   glColor3f(1.0, 1.0, 0.0);
+   glTranslatef(-2+4*sunRadius, 1-3*sunRadius, -3.0);
+   glutSolidSphere(sunRadius, 100, 100);
+
+   //SEA
+   glLoadIdentity();
+   glColor3f(0.31,0.26,1.0);
+   glBegin(GL_POLYGON);
+      glVertex3f(2, -0.5, -5);
+      glVertex3f(-2, -0.5, -5);
+      glVertex3f(-2, -1, -5);
+      glVertex3f(2, -1, -5);
+   glEnd();
+
+   //GRASS
+   glLoadIdentity();
+   glColor3f(0.6,0.8,0.196078);
+   glBegin(GL_POLYGON);
+      glVertex3f(2, -0.3, -5);
+      glVertex3f(-2, 0.0, -5);
+      glVertex3f(-2, -0.5, -5);
+      glVertex3f(2, -0.5, -5);
+   glEnd();
+
+   //SKY
+   glLoadIdentity();
+   glColor3f(0.53,0.81,0.92);
+   glBegin(GL_POLYGON);
+      glVertex3f(2, 1, -5);
+      glVertex3f(-2, 1, -5);
+      glVertex3f(-2, 0.0, -5);
+      glVertex3f(2, -0.3, -5);
+   glEnd();
+
+   Cloud();
    glutSwapBuffers();
 }
 
